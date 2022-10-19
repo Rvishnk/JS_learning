6fae7d61a17ea21b38390c4f1001db7c2aaf3a80
ravi shankar <rvishnkr165@gmail.com>
2022-10-19 23:17:42 +0530

    test msg

---
+++ UnsungBulkyEquipment/index.js
@@ -0,0 +1,9 @@
+//Given a string print, the number of times each character appears
+let str="School";
+let number=0;
+let obj={};
+for(let i=0;i<str.length;i++){
+  obj[str[i]]=Number
+  number++
+}
+console.log(obj)
